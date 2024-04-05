5d7e142368330d4d9b71865b8153f42cfc7e4128
Fatimah Al Mousa <fatimah.almousa1424@gmail.com>
2024-04-05 15:09:59 +0300

    <full project hangman game>

--- src/guess.dart
+++ src/guess.dart
@@ -1,5 +1,29 @@
+// ignore_for_file: unused_local_variable
+
 /* String getValidGuess() {
 -The getValidGuess function prompts the player to enter a guess.
 -It uses a loop to ensure that the input is a valid guess.
 -It reads the input from the user, trims and converts it to lowercase, and validates it using a regular expression.
 } */
+
+import 'dart:io';
+
+String getValidGuess() {
+  stdout.write("Enter your guess (a single letter): ");
+  String? guess = stdin.readLineSync();
+
+  while (guess == null ||
+      guess.isEmpty ||
+      guess.length != 1 ||
+      !RegExp(r'[a-zA-Z]').hasMatch(guess)) {
+    print("Invalid guess. Please enter a single letter.");
+    stdout.write("Enter your guess (a single letter): ");
+    guess = stdin.readLineSync();
+  }
+
+  return guess.toLowerCase();
+}
+
+String chooseRandomWord(List<String> words) {
+  return words[DateTime.now().microsecondsSinceEpoch % words.length];
+}
--- src/hangman.dart
+++ src/hangman.dart
@@ -1,6 +1,37 @@
+import 'guess.dart';
 
 void hangman() {
   print("Welcome to Hangman!");
+  List<String> words = ["flutter", "banana", "hangman"];
+  String guessed = "hangman";
+  String guessedWord = chooseRandomWord(words);
+  List<String> guessedLetters = [];
+  int attemptsLeft = 6;
+
+  while (attemptsLeft > 0) {
+    print(
+        "Word: ${guessedWord.split('').map((char) => guessedLetters.contains(char) ? char : '_').join(' ')}");
+    print("Guessed Letters: ${guessedLetters.join(', ')}");
+    print("Attempts Left: $attemptsLeft");
+
+    String guess = getValidGuess();
+
+    if (guessedWord.contains(guess)) {
+      print("$guess\nCorrect!");
+      guessedLetters.add(guess);
+    } else {
+      print("Incorrect");
+      attemptsLeft--;
+      print("Attempts Left: $attemptsLeft");
+    }
+
+    if (guessedWord.split('').every((char) => guessedLetters.contains(char))) {
+      print("Congratulations! You guessed the word: $guessedWord");
+      return;
+    }
+  }
+
+  print("You ran out of attempts. The word was: $guessedWord");
 
   //List<String> words = []; Start with a list of any words you wish to add
   //choose random word from the list as the guessed word
